38838f28f64178b15eefd143d8dda08d5282b6fe
ujjwal G <crystalninja0123@gmail.com>
2025-10-22 20:17:37 +0530

    type fix

--- src/app/components/slides.tsx
+++ src/app/components/slides.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import image from "next/image"
+import Image from "next/image"
 export default function CardSlides() {
   const router = useRouter();
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
